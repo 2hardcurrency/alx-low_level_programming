13e562cd4bd7e18d018cf0d64beb1d7012379347
2hardcurrency <sinafavo@gmail.com>
2022-12-08 11:36:28 -0800

    my first Clanguage

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - print aline of codes with puts
  *
- * return: 0
+ * Return: 0
 */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	return (0);
 }
