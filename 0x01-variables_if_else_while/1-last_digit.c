1b9a041dfe1b78e4e7b4cefe3773b06a1ed926d4
2hardcurrency <sinafavo@gmail.com>
2022-12-09 20:50:09 -0800

    my task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,8 +9,8 @@
 */
 int main(void)
 {
-int n;
-int x;
+                int n;
+                int x;
 
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
@@ -27,5 +27,6 @@ int x;
 		if (x < 6 && x != 0)
 		{
 			printf("and is less than 6 and not equal to 0");
+		}
 		return (0);
 }
