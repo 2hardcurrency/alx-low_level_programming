88fca43dc5be1027853bb6da0a9dd3b07407303e
2hardcurrency <sinafavo@gmail.com>
2022-12-10 14:33:20 -0800

    My task 3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - to write a program that prints the alphabets in lower case
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}	
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - to write code that prints alphabet in lowercase, then uppercase
+ *
+ * Return: 0
+*/
+int main(void)
+
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar('a');
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar('A');
+
+	putchar('\n');
+
+	return (0);
+
+}
