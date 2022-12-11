ca7d3994d006bd354ec6b724e9d0e24ed5ad85bb
2hardcurrency <sinafavo@gmail.com>
2022-12-11 11:47:16 -0800

    my commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - to write a program that prints all single digit numbers of base 10
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar ("%d", n);
+
+		putchar ('\n');
+		return (0);
+}
