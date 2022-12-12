81ec74607aa4917d7c82b2d765d33b942698be27
2hardcurrency <sinafavo@gmail.com>
2022-12-12 14:34:09 -0800

    my task3

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,7 +9,7 @@ int main(void)
 	int d1;
 	int d2;
 
-	for (di = 0; d1 < 100; d1++)
+	for (d1 = 0; d1 < 100; d1++)
 	{
 		for (d2 = 0; d2 < 100; d2++)
 		{
@@ -21,7 +21,7 @@ int main(void)
 				putchar((d2 / 10) + 48);
 				putchar((d2 % 10) + 48);
 
-				if (d1 != 98 || d2 !99)
+				if (d1 != 98 || d2 != 99)
 				{
 					putchar(',');
 					putchar(' ');
