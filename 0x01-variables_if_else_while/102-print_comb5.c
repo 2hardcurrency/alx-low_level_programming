831278f0a2c1cd0eb3a37f44d0d7078c700823b9
2hardcurrency <sinafavo@gmail.com>
2022-12-12 14:29:31 -0800

    my task3

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio>
+/**
+ * main - print two two digits combinattions
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int d1;
+	int d2;
+
+	for (di = 0; d1 < 100; d1++)
+	{
+		for (d2 = 0; d2 < 100; d2++)
+		{
+			if (d1 < d2)
+			{
+				putchar((d1 / 10) + 48);
+				putchar((d1 % 10) + 48);
+				putchar(' ');
+				putchar((d2 / 10) + 48);
+				putchar((d2 % 10) + 48);
+
+				if (d1 != 98 || d2 !99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
