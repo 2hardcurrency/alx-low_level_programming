1f56c3fa4e8958f70e0555fff37035935623f023
2hardcurrency <sinafavo@gmail.com>
2022-12-13 13:29:28 -0800

    my task 00

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Result: Always 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter < 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+
+	return (0);
+}
