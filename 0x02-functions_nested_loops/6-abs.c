e0bc5dc3076abf6eb917e4fa3f378266cc1510c2
2hardcurrency <sinafavo@gmail.com>
2022-12-13 18:08:29 -0800

    my task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - print the abs value of a number
+ * Return: absolute value
+ *@n: the int in the question
+ */
+
+int _abs(int n);
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
+
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit of a number
+ *
+ * Return: last digit
+ */
+int print_last_digit(int)
+{
+	int n;
+	int i;
+
+	i = n % 10;
+
+	_putchar('i');
+
+	return (i);
+}
