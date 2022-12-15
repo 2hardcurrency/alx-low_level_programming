89e74c1431b8c37a081e9c83e5eb9ae86ed0b2fe
2hardcurrency <sinafavo@gmail.com>
2022-12-15 07:47:41 -0800

    adding digts

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adding two numbers
+ * @num1: fisrt integer
+ * @num2: second integer
+ * Return: the result of the two integers
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
