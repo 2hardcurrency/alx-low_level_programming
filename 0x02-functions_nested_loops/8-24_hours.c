0a6e32082dd92c4f1e3e53b52762679b2636b896
2hardcurrency <sinafavo@gmail.com>
2022-12-15 08:38:48 -0800

    my commmit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,8 +3,6 @@
 /**
  * jack_bauer - function that prints every minute of the day of Jack Bauer
  *
- * Return: Always 0
- *
  */
 void jack_bauer(void)
 {
@@ -15,13 +13,13 @@ void jack_bauer(void)
 	{
 		for (m = 0; m <= 59; m++)
 		{
-				_putchar((h / 10) + '0');
-				_putchar((h % 10) + '0');
-				_putchar(':');
-				_putchar((m / 10) + '0');
-				_putchar((m % 10) + '0');
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
 
-				_putchar('\n');
+			_putchar('\n');
 			}
 		}
 }
