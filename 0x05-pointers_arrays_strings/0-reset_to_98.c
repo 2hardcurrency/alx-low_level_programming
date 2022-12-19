cbc72aea10d359bf80463777cd8f418748d3be15
2hardcurrency <sinafavo@gmail.com>
2022-12-19 04:08:05 -0800

    my first pointer tast

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - to write  function that updates a function that it points
+ * @n: pointer
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	int *n;
+
+	n = 98;
+	return (0);
+}
