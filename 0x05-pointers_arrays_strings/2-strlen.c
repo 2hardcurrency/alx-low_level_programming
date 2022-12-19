e4215221e5ce307278b25df44dc1111de6cf78a2
2hardcurrency <sinafavo@gmail.com>
2022-12-19 06:39:57 -0800

    my commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen function to display the lenght of string
+ *
+ * Return - (0)
+ */
+int _strlen(char *s)
+{
+	_putchar("%s", strlen(int, *s));
+
+		return (0);
+}
