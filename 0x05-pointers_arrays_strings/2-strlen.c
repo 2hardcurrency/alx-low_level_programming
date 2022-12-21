04af3678861fa9cc9c285bc786d2b76cefd9d3b8
2hardcurrency <sinafavo@gmail.com>
2022-12-21 02:47:18 -0800

    my string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,17 +2,17 @@
 
 /**
  * _strlen - function to display the lenght of string
- * @s: string to be checked
- * @_strlen: lenght of string
- * Return - (0)
+ * @s: String to be chkecked
+ * Return: 0
  */
+
 int _strlen(char *s)
 {
 	int counter;
 
 	counter = 0;
 
-	for (;; *s++)
+	for (; *s++;)
 		counter++;
 
 	return (counter);
