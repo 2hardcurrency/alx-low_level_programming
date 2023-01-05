76979b894eb130ec3461c9639c0a70810369a28f
2hardcurrency <sinafavo@gmail.com>
2023-01-04 16:24:21 -0800

    recursion task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string
+ *
+ * @s: the string to be printed
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+	char s[]
+	{
+		char s;
+
+		scanf("%s",  s);
+		_putchar("%s", s);
+
+		return (str);
+	}
+}
