fdfe1c0845a723b1283bb7d1afc7e3faa9a4dd11
2hardcurrency <sinafavo@gmail.com>
2023-01-15 07:45:47 -0800

    my task

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+i#include "main.h"
+
+/**
+ * _strlen_recursion - calculating lenght of string
+ * @s: lenght of string char
+ * Return: string lenght
+ *
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
