ed3039f75a8dbeb5c54b496d779f0ce68d11b6aa
2hardcurrency <sinafavo@gmail.com>
2023-01-15 12:44:58 -0800

    My commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _pow_recursion - function that returns 'x' exponent of 'y'
+ * @x: int 1
+ * @y: int 2
+ * Return: int
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	else if (y > 0)
+	{
+		return (x * (x, y - 1));
+	}
+	else
+	{
+		return (1);
+	}
+}
