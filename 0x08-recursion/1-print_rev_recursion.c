654f92cdbf457eaab8447fce5618ec8e1760c3fa
2hardcurrency <sinafavo@gmail.com>
2023-01-15 06:08:35 -0800

    my commit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
  * __print_rev_recursion - reversed string
- *@s: string
+ * @s: string
  * Return always *s
+ * i
  */
 void _print_rev_recursion(char *s);
 {
@@ -10,5 +11,6 @@ void _print_rev_recursion(char *s);
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
+		_print_rev_recursion(s - 1);
 	}
 }
