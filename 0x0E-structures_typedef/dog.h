bd16ba9cfaa4304962c0392c1a2ee6a0bdef57f0
2hardcurrency <sinafavo@gmail.com>
2023-01-16 15:42:48 -0800

    my task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,4 @@
-#ifndef DOG
+i#ifndef DOG
 #define DOG
 
 /**
@@ -16,15 +16,5 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-
-typedef struct dog dog_t;
-
-dog_t *new_dog(char *name, float age, char *owner);
-
-void free_dog(dog_t *d);
-
+}
 #endif
