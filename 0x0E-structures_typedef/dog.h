558ecb6844fc34f75e987bc20e4fed21170613a3
2hardcurrency <sinafavo@gmail.com>
2023-01-16 15:37:37 -0800

    my task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,30 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
+#ifndef DOG
+#define DOG
 
 /**
  * struct dog - dog class
  *
- * @name: chararcter name
+ * @name: Name of the dog
  *
- * @owner: character owner
+ * @age: Age of the dog
  *
- * @age: float number
+ * @owner: Owner of the dog
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
 #endif
