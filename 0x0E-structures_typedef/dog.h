bae02607324c4ce9214b72c69e042bc55e64a647
2hardcurrency <sinafavo@gmail.com>
2023-01-16 12:52:49 -0800

    my commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef "MAIN_H"
+#define "MAIN_H"
+
+/**
+ * struct dog - dog class
+ *
+ * @name: chararcter name
+ *
+ * @owner: character owner
+ *
+ * @age: float number
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+int main(void)
+{
+	struct dog dog(char *name, float age, char *owner)
+
+		return (0);
+}
+
+#endif
