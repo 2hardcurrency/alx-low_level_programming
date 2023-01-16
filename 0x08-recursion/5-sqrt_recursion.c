0e309ddfebd6c56e9b561fb4b3fc4dd3546369cb
2hardcurrency <sinafavo@gmail.com>
2023-01-15 16:00:18 -0800

    my commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - to return the square root of numbers
+ * @i: sqr rt of n
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	int i;
+
+	if (i * i = n)
+	{
+		return n;
+	}
+	else if (i * i > n)
+	{
+		return (-1);
+	}
+
+
