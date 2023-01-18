f5456233123446daa7ce8a1a724ac31647316311
2hardcurrency <sinafavo@gmail.com>
2023-01-18 04:05:20 -0800

    my commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,9 +3,14 @@
 
 /**
  *print_name - to write function that prints a name
+ *
+ * @name: arguement of the function
+ * @f: pointer to the function
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 	char print_name[20] = "name";
+
 	printf("%s", print_name);
+}
