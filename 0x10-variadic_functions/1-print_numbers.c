eb7f945f58ba2c6b510aee29a8e3a0c6e5d1d4e4
2hardcurrency <sinafavo@gmail.com>
2023-01-19 14:37:42 -0800

    My commit

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers -  function that prints numbers
+ * @n: the number of parameters passed
+ * Return: Always 0
+ * 
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	const unsigned int i;
+
+	va_list ap;
+
+	va_start(ap, n);
+
+	if (seperator == NULL)
+		seperator = "";
+
+	for (i = 0; i < n; i++)
+	printf("%d", va_arg(ap, int));
+
+	for (i < n - 1)
+		printf("%s", seperator);
+
+	printf("\n");
+
+	va_end(ap);
+}
+
+
