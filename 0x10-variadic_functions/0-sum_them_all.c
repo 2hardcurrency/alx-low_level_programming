ac44d3f2bd5adafb8d46ece0e19ab159273d93d6
2hardcurrency <sinafavo@gmail.com>
2023-01-19 13:15:33 -0800

    variant

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,17 +10,20 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int i, sum = 0;
+	unsigned int i;
+	int sum;
 
 	va_list list;
 
-	va_start(list
+	va_start(list, n);
+
+	if (n == 0)
+		return (0);
 	for (i = 0; i < n; i++)
 		(
 		 sum += va_arg(list, int);
 		 )
-			if (n == 0)
-				return (0);
+
 	va_end(list);
 
 	return (sum);
