814e65bf4ec7f362b2a8f34ec21c8b6f9962704f
2hardcurrency <sinafavo@gmail.com>
2023-01-19 16:01:49 -0800

    my commit

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - function to print string
+ *
+ * @n:number of strings
+ *
+ * @separator: A string that is used to separate strings
+ *
+ * Return: string
+ */
+void print_strings(const char *separator, const unsigned int n, ...);
+{
+	unsigned int i;
+
+	va_list ap;
+
+	va_start(ap, n);
+
+	if (seperator == NULL)
+		seperator = "";
+
+	if (str == NULL)
+		str = ("nil");
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%s", va_arg(ap, char *);
+				if (i < n - 1)
+				printf("seperator");
+				}
+				printf("\n");
+				va_end(ap);
+				}
