0d1c712e4c72694476c8359a74201489f35379f3
2hardcurrency <sinafavo@gmail.com>
2023-01-28 15:06:55 -0800

    updated recursions

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,21 +1,20 @@
 #include "main.h"
 /**
- * _pow_recursion - function that returns 'x' exponent of 'y'
- * @x: int 1
- * @y: int 2
- * Return: int
+ * _pow_recursion - power
+ * @x:int
+ * @y:int
+ * Return:int
  */
+
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 	{
 		return (-1);
 	}
-
 	else if (y != 0)
-	{
-		return (x * (x, y - 1));
-	}
+		return (x * _pow_recursion(x, y - 1));
+
 	else
 	{
 		return (1);
