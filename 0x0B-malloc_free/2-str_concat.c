6bb58be1a4bc3e8973d308da6a542bfe432f3c23
2hardcurrency <sinafavo@gmail.com>
2023-01-31 13:14:36 -0800

    my commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *str_concat - function that concats two strings
+ *
+ * Return: string
+ *
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int l1, l2, i;
+	l1 = strlen(s1);
+	l2 = strlen(s2);
+
+	for (i = 0; i < l2; i++)
+	{
+		s1[l1 + i] = s2[i];
+	}
+
+	printf("%s", s1);
+}
