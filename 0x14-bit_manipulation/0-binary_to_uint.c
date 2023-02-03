e85dc7a768d2ca9a7d336b3843c4843c159f5739
2hardcurrency <sinafavo@gmail.com>
2023-02-03 06:18:51 -0800

    commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * binary_to_uint - function that converts a binary number
+ *
+ * @b: pointing to a string of 0 and 1 chars
+ *
+ * Return: 0 or unsigned int
+ *
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int n;
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+	if (b[i] != '1' && b[i] != '0')
+		return (0);
+	}
+
+	if (b[i] == NULL)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+
+		n <<= 1;
+	if (b[i] == '1')
+		n += 1;
+	}
+
+	return (n);
+}
+
