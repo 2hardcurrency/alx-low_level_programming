0d5cbc05d54109259b2e53c6570dac659c8a54d7
2hardcurrency <sinafavo@gmail.com>
2023-02-06 14:20:14 -0800

    filling

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,32 +1,39 @@
 #include "main.h"
-#include <xtdio.h>
 #include <stdlib.h>
 
 /**
- * read_textfile -  function that reads a text file and prints it 
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
  *
- * Return: 0
- * @letter: string to be printed
- *
- * @fp: pointer that stores the address
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *fp;
-	char letter[20];
-
-	fp = fopen("filename", "r");
+	ssize_t o, r, w;
+	char *buffer;
 
 	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
+		free(buffer);
 		return (0);
 	}
 
-	if (fp == NULL)
-		return (0);
+	free(buffer);
+	close(o);
 
-	printf("%s", fgets(letter, letters), fp);
-		fclose(fp);
-
-		return (0);
+	return (w);
 }
