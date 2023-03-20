bc99e2861d2bf365f1ed74ea9367889d70dfe2bb
2hardcurrency <sinafavo@gmail.com>
2023-03-19 18:50:21 -0700

    dog ini

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,5 +12,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *d = {"dog", 3.3, "owner"}	
-}
+	struct dog *d {
+		char *name;
+		float age;
+		char *owner;
+};
