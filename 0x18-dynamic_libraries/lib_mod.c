8c84b7c291bc97067938ffae7d0d100a0fc4350e
2hardcurrency <sinafavo@gmail.com>
2023-03-21 19:03:54 -0700

    dynamic library

---
+++ 0x18-dynamic_libraries/lib_mod.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void mod(int a, int b)
+{
+       printf("mod = %d", a % b == 0);
+}
---
+++ 0x18-dynamic_libraries/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+int main()
+{
+	int a = 30;
+	int b = 10;
+	add(a, b);
+	div(a, b);
+	mult(a, b);
+	sub(a, b);
+	mod(a, b);
+
+	return(0);
+}
