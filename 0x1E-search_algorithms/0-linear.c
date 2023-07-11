ee53bf3765606d08e5b1f4bdeb3a1f0e14b69c08
2hardcurrency <sinafavo@gmail.com>
2023-07-11 14:43:36 -0700

    commit

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,8 +13,11 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
+	size_t i = 0;
+
 	if (size == 0)
 		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
