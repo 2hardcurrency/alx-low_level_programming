9dbfd466a5e7bffa3a955778af5ad9071d1614f4
2hardcurrency <sinafavo@gmail.com>
2023-07-12 04:14:55 -0700

    task

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,31 +14,27 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i, m, right, left;
-
-	size = len
-	left = 0;
-	right = size - 1;
-
+	size_t i, left, right;
 
 	if (array == NULL)
 		return (-1);
 
-	while (left < right)
+	for (left = 0, right = size - 1; right >= left;)
 	{
-		printf("Searching in array: ")
+		printf("Searching in array: ");
 		for (i = left; i < right; i++)
-			printf("%d ", array[i]);
-		printf("%d", array[m]);
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
 
-		m = left + (right - left) / 2;
-		if (array[m] == value)
-			return (m);
-		if (array[m] < value)
-			left = m + 1;
+		i = left + (right - left) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			right = i - 1;
 		else
-			right = m - 1;
+			left = i + 1;
 	}
 
 	return (-1);
 }
+
